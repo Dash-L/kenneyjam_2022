101a5d2ba82179c80500a5d47bc68d91a9198046
Dashiell Elliott <dashiell.elliott@yahoo.com>
2022-08-19 12:16:17 -0400

    some boilerplate + background image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use bevy::{prelude::*, render::texture::ImageSettings};
+use bevy_asset_loader::prelude::*;
+use iyes_loopless::prelude::*;
+
+mod consts;
+use consts::*;
+
+mod helpers;
+use helpers::*;
+
+mod plugins;
+use plugins::*;
+
+mod resources;
+use resources::*;
+
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+pub enum GameState {
+    Load,
+    Setup,
+    InGame,
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ImageSettings::default_nearest())
+        .insert_resource(WindowDescriptor {
+            title: "Kenney Jam".to_string(),
+            width: WIDTH,
+            height: HEIGHT,
+            resizable: false,
+            ..default()
+        })
+        .add_loopless_state(GameState::Load)
+        .add_loading_state(
+            LoadingState::new(GameState::Load)
+                .continue_to_state(GameState::Setup)
+                .with_collection::<Sprites>(),
+        )
+        .add_plugins(DefaultPlugins)
+        .add_enter_system(GameState::Setup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, sprites: Res<Sprites>) {
+    commands.spawn_bundle(Camera2dBundle::default());
+    commands.spawn_bundle(SpriteBundle {
+        texture: sprites.background.clone(),
+        transform: Transform::from_scale(Vec3::splat(5.0)),
+        ..default()
+    });
 }
---
+++ src/resources.rs
@@ -0,0 +1,36 @@
+use bevy::prelude::*;
+use bevy_asset_loader::prelude::*;
+
+#[derive(AssetCollection)]
+pub struct Sprites {
+    #[asset(path = "sprites/player.png")]
+    pub player: Handle<Image>,
+    #[asset(path = "sprites/archer.png")]
+    pub archer: Handle<Image>,
+    #[asset(path = "sprites/alchemist.png")]
+    pub alchemist: Handle<Image>,
+    #[asset(path = "sprites/cyclops.png")]
+    pub cyclops: Handle<Image>,
+    #[asset(path = "sprites/lobster.png")]
+    pub lobster: Handle<Image>,
+    #[asset(path = "sprites/dwarf.png")]
+    pub dwarf: Handle<Image>,
+    #[asset(path = "sprites/ghost.png")]
+    pub ghost: Handle<Image>,
+    #[asset(path = "sprites/knight.png")]
+    pub knight: Handle<Image>,
+    #[asset(path = "sprites/evil_wizard.png")]
+    pub evil_wizard: Handle<Image>,
+    #[asset(path = "sprites/bat.png")]
+    pub bat: Handle<Image>,
+    #[asset(path = "sprites/cactus.png")]
+    pub cactus: Handle<Image>,
+    #[asset(path = "sprites/rat1.png")]
+    pub rat1: Handle<Image>,
+    #[asset(path = "sprites/rat2.png")]
+    pub rat2: Handle<Image>,
+    #[asset(path = "sprites/wizard.png")]
+    pub wizard: Handle<Image>,
+    #[asset(path = "sprites/background.png")]
+    pub background: Handle<Image>,
+}
