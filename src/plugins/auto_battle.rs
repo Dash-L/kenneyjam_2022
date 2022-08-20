fe5b71a992df8207f1915b3979cef388b9de0f96
Dashiell Elliott <dashiell.elliott@yahoo.com>
2022-08-20 13:35:00 -0400

    less shit auto battle plugin

--- src/plugins/auto_battle.rs
+++ src/plugins/auto_battle.rs
@@ -25,18 +25,8 @@ impl Plugin for AutoBattlePlugin {
                     .with_system(auto_battle::<EnemyType, AllyType>)
                     .with_system(collide_projectiles::<AllyType, EnemyType>)
                     .with_system(collide_projectiles::<EnemyType, AllyType>)
-                    .with_system(handle_archer_attack)
-                    .with_system(handle_cyclops_attack)
-                    .with_system(handle_dwarf_attack)
-                    .with_system(handle_knight_attack)
-                    .with_system(handle_player_attack)
-                    .with_system(handle_wizard_attack)
-                    .with_system(handle_bat_attack)
-                    .with_system(handle_evil_wizard_attack)
-                    .with_system(handle_ghost_attack)
-                    .with_system(handle_lobster_attack)
-                    .with_system(handle_rat_attack)
-                    .with_system(handle_spider_attack)
+                    .with_system(handle_ally_attacks)
+                    .with_system(handle_enemy_attacks)
                     .with_system(move_projectiles)
                     .into(),
             );
@@ -97,218 +87,103 @@ fn collide_projectiles<A, T>(
     }
 }
 
-fn handle_cyclops_attack(
-    mut attack_events: EventReader<AttackEvent<AllyType>>,
-    cyclops_q: Query<(&Transform, &Damage), With<AllyType>>,
-    enemy_q: Query<&Transform, With<EnemyType>>,
-) {
-    for AttackEvent(_, cyclops_entity, enemy_entity) in
-        attack_events.iter().filter(|ev| ev.0 == AllyType::Cyclops)
-    {
-        let cyclops = cyclops_q.get(*cyclops_entity);
-        let enemy = enemy_q.get(*enemy_entity);
-    }
-}
-
-fn handle_archer_attack(
+fn handle_ally_attacks(
     mut commands: Commands,
     sprites: Res<Sprites>,
     mut attack_events: EventReader<AttackEvent<AllyType>>,
-    archer_q: Query<(&Transform, &Damage), With<AllyType>>,
-    enemy_q: Query<&Transform, With<EnemyType>>,
+    allies: Query<(&Transform, &Damage, &AllyType)>,
+    enemies: Query<&Transform, With<EnemyType>>,
 ) {
-    for AttackEvent(_, archer_entity, enemy_entity) in
-        attack_events.iter().filter(|ev| ev.0 == AllyType::Archer)
-    {
-        let (transform, damage) = archer_q.get(*archer_entity).unwrap();
-        let other_transform = enemy_q.get(*enemy_entity).unwrap();
+    for AttackEvent(_, ally_entity, enemy_entity) in attack_events.iter() {
+        if let Ok((ally_transform, damage, ally_type)) = allies.get(*ally_entity) {
+            if let Ok(enemy_transform) = enemies.get(*enemy_entity) {
+                match ally_type {
+                    AllyType::Archer => {
+                        let dir = (enemy_transform.translation.truncate()
+                            - ally_transform.translation.truncate())
+                        .normalize();
 
-        let dir =
-            (other_transform.translation.truncate() - transform.translation.truncate()).normalize();
-
-        commands
-            .spawn_bundle(ProjectileBundle {
-                speed: Speed(20.0),
-                velocity: Velocity(dir),
-                damage: Damage(damage.0),
-                projectile: Projectile::<AllyType>::default(),
-                sprite: SpriteBundle {
-                    texture: sprites.arrow.clone(),
-                    transform: Transform::from_translation(transform.translation)
-                        .with_rotation(Quat::from_rotation_z(Vec2::Y.angle_between(dir)))
-                        .with_scale(Vec3::splat(1.5)),
-                    ..default()
-                },
-            })
-            .insert(Collider(Vec2::new(8., 16.) * 1.5));
+                        commands
+                            .spawn_bundle(ProjectileBundle {
+                                speed: Speed(20.0),
+                                velocity: Velocity(dir),
+                                damage: Damage(damage.0),
+                                projectile: Projectile::<AllyType>::default(),
+                                sprite: SpriteBundle {
+                                    texture: sprites.arrow.clone(),
+                                    transform: Transform::from_translation(
+                                        ally_transform.translation,
+                                    )
+                                    .with_rotation(Quat::from_rotation_z(
+                                        Vec2::Y.angle_between(dir),
+                                    ))
+                                    .with_scale(Vec3::splat(1.5)),
+                                    ..default()
+                                },
+                            })
+                            .insert(Collider(Vec2::new(8., 16.) * 1.5));
+                    }
+                    AllyType::Wizard => {
+                        commands.spawn_bundle(ProjectileBundle {
+                            speed: Speed(20.0),
+                            velocity: Velocity(
+                                (enemy_transform.translation.truncate()
+                                    - ally_transform.translation.truncate())
+                                .normalize(),
+                            ),
+                            damage: Damage(damage.0),
+                            projectile: Projectile::<AllyType>::default(),
+                            sprite: SpriteBundle {
+                                texture: sprites.fireball.clone(),
+                                transform: Transform::from_translation(ally_transform.translation),
+                                ..default()
+                            },
+                        });
+                    }
+                    _ => {}
+                }
+            }
+        }
     }
 }
 
-fn handle_dwarf_attack(
-    mut attack_events: EventReader<AttackEvent<AllyType>>,
-    dwarf_q: Query<(&Transform, &Damage), With<AllyType>>,
-    enemy_q: Query<&Transform, With<EnemyType>>,
-) {
-    for AttackEvent(_, dwarf_entity, enemy_entity) in
-        attack_events.iter().filter(|ev| ev.0 == AllyType::Dwarf)
-    {
-        let dwarf = dwarf_q.get(*dwarf_entity);
-        let enemy = enemy_q.get(*enemy_entity);
-    }
-}
-
-fn handle_knight_attack(
-    mut attack_events: EventReader<AttackEvent<AllyType>>,
-    knight_q: Query<(&Transform, &Damage), With<AllyType>>,
-    enemy_q: Query<&Transform, With<EnemyType>>,
-) {
-    for AttackEvent(_, knight_entity, enemy_entity) in
-        attack_events.iter().filter(|ev| ev.0 == AllyType::Knight)
-    {
-        let knight = knight_q.get(*knight_entity);
-        let enemy = enemy_q.get(*enemy_entity);
-    }
-}
-
-fn handle_player_attack(
-    mut attack_events: EventReader<AttackEvent<AllyType>>,
-    player_q: Query<(&Transform, &Damage), With<AllyType>>,
-    enemy_q: Query<&Transform, With<EnemyType>>,
-) {
-    for AttackEvent(_, player_entity, enemy_entity) in
-        attack_events.iter().filter(|ev| ev.0 == AllyType::Player)
-    {
-        let player = player_q.get(*player_entity);
-        let enemy = enemy_q.get(*enemy_entity);
-    }
-}
-
-fn handle_wizard_attack(
-    mut commands: Commands,
-    sprites: Res<Sprites>,
-    mut attack_events: EventReader<AttackEvent<AllyType>>,
-    wizard_q: Query<(&Transform, &Damage), With<AllyType>>,
-    enemy_q: Query<&Transform, With<EnemyType>>,
-) {
-    for AttackEvent(_, wizard_entity, enemy_entity) in
-        attack_events.iter().filter(|ev| ev.0 == AllyType::Wizard)
-    {
-        let (transform, damage) = wizard_q.get(*wizard_entity).unwrap();
-        let other_transform = enemy_q.get(*enemy_entity).unwrap();
-
-        commands.spawn_bundle(ProjectileBundle {
-            speed: Speed(20.0),
-            velocity: Velocity(
-                (other_transform.translation.truncate() - transform.translation.truncate())
-                    .normalize(),
-            ),
-            damage: Damage(damage.0),
-            projectile: Projectile::<AllyType>::default(),
-            sprite: SpriteBundle {
-                texture: sprites.fireball.clone(),
-                transform: Transform::from_translation(transform.translation),
-                ..default()
-            },
-        });
-    }
-}
-
-fn handle_bat_attack(
-    mut attack_events: EventReader<AttackEvent<EnemyType>>,
-    bat_q: Query<(&Transform, &Damage), With<EnemyType>>,
-    ally_q: Query<&Transform, With<AllyType>>,
-) {
-    for AttackEvent(_, bat_entity, ally_entity) in
-        attack_events.iter().filter(|ev| ev.0 == EnemyType::Bat)
-    {
-        let bat = bat_q.get(*bat_entity);
-        let ally = ally_q.get(*ally_entity);
-    }
-}
-
-fn handle_evil_wizard_attack(
+fn handle_enemy_attacks(
     mut commands: Commands,
     sprites: Res<Sprites>,
     mut attack_events: EventReader<AttackEvent<EnemyType>>,
-    evil_wizard_q: Query<(&Transform, &Damage), With<EnemyType>>,
-    ally_q: Query<&Transform, With<AllyType>>,
+    enemies: Query<(&Transform, &Damage, &EnemyType)>,
+    allies: Query<&Transform, With<AllyType>>,
 ) {
-    for AttackEvent(_, evil_wizard_entity, ally_entity) in attack_events
-        .iter()
-        .filter(|ev| ev.0 == EnemyType::EvilWizard)
-    {
-        let (transform, damage) = evil_wizard_q.get(*evil_wizard_entity).unwrap();
-        let other_transform = ally_q.get(*ally_entity).unwrap();
-
-        commands
-            .spawn_bundle(ProjectileBundle {
-                speed: Speed(20.0),
-                velocity: Velocity(
-                    (other_transform.translation.truncate() - transform.translation.truncate())
-                        .normalize(),
-                ),
-                damage: Damage(damage.0),
-                projectile: Projectile::<EnemyType>::default(),
-                sprite: SpriteBundle {
-                    texture: sprites.fireball.clone(),
-                    transform: Transform::from_translation(transform.translation)
-                        .with_scale(Vec3::splat(1.5)),
-                    ..default()
-                },
-            })
-            .insert(Collider(Vec2::new(8., 8.) * 1.5));
-    }
-}
-
-fn handle_ghost_attack(
-    mut attack_events: EventReader<AttackEvent<EnemyType>>,
-    ghost_q: Query<(&Transform, &Damage), With<EnemyType>>,
-    ally_q: Query<&Transform, With<AllyType>>,
-) {
-    for AttackEvent(_, ghost_entity, ally_entity) in
-        attack_events.iter().filter(|ev| ev.0 == EnemyType::Ghost)
-    {
-        let ghost = ghost_q.get(*ghost_entity);
-        let ally = ally_q.get(*ally_entity);
-    }
-}
-
-fn handle_lobster_attack(
-    mut attack_events: EventReader<AttackEvent<EnemyType>>,
-    lobster_q: Query<(&Transform, &Damage), With<EnemyType>>,
-    ally_q: Query<&Transform, With<AllyType>>,
-) {
-    for AttackEvent(_, lobster_entity, ally_entity) in
-        attack_events.iter().filter(|ev| ev.0 == EnemyType::Lobster)
-    {
-        let lobster = lobster_q.get(*lobster_entity);
-        let ally = ally_q.get(*ally_entity);
-    }
-}
-
-fn handle_rat_attack(
-    mut attack_events: EventReader<AttackEvent<EnemyType>>,
-    rat_q: Query<(&Transform, &Damage), With<EnemyType>>,
-    ally_q: Query<&Transform, With<AllyType>>,
-) {
-    for AttackEvent(_, rat_entity, ally_entity) in
-        attack_events.iter().filter(|ev| ev.0 == EnemyType::Rat)
-    {
-        let rat = rat_q.get(*rat_entity);
-        let ally = ally_q.get(*ally_entity);
-    }
-}
-
-fn handle_spider_attack(
-    mut attack_events: EventReader<AttackEvent<EnemyType>>,
-    spider_q: Query<(&Transform, &Damage), With<EnemyType>>,
-    ally_q: Query<&Transform, With<AllyType>>,
-) {
-    for AttackEvent(_, spider_entity, ally_entity) in
-        attack_events.iter().filter(|ev| ev.0 == EnemyType::Spider)
-    {
-        let spider = spider_q.get(*spider_entity);
-        let ally = ally_q.get(*ally_entity);
+    for AttackEvent(_, enemy_entity, ally_entity) in attack_events.iter() {
+        if let Ok((enemy_transform, damage, enemy_type)) = enemies.get(*enemy_entity) {
+            if let Ok(ally_transform) = allies.get(*ally_entity) {
+                match enemy_type {
+                    EnemyType::EvilWizard => {
+                        commands
+                            .spawn_bundle(ProjectileBundle {
+                                speed: Speed(20.0),
+                                velocity: Velocity(
+                                    (ally_transform.translation.truncate()
+                                        - enemy_transform.translation.truncate())
+                                    .normalize(),
+                                ),
+                                damage: Damage(damage.0),
+                                projectile: Projectile::<EnemyType>::default(),
+                                sprite: SpriteBundle {
+                                    texture: sprites.fireball.clone(),
+                                    transform: Transform::from_translation(
+                                        enemy_transform.translation,
+                                    )
+                                    .with_scale(Vec3::splat(1.5)),
+                                    ..default()
+                                },
+                            })
+                            .insert(Collider(Vec2::new(8., 8.) * 1.5));
+                    }
+                    _ => {}
+                }
+            }
+        }
     }
 }
 
