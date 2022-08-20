bd4f753557229e50f4068d1cb9a2bcbf28b367a8
Dashiell Elliott <dashiell.elliott@yahoo.com>
2022-08-19 23:20:48 -0400

    rotate arrows

--- src/plugins/auto_battle.rs
+++ src/plugins/auto_battle.rs
@@ -76,8 +76,8 @@ fn handle_cyclops_attack(
     cyclops_q: Query<(&Transform, &Damage), With<AllyType>>,
     enemy_q: Query<&Transform, With<EnemyType>>,
 ) {
-    if let Some(AttackEvent(_, cyclops_entity, enemy_entity)) =
-        attack_events.iter().find(|ev| ev.0 == AllyType::Cyclops)
+    for AttackEvent(_, cyclops_entity, enemy_entity) in
+        attack_events.iter().filter(|ev| ev.0 == AllyType::Cyclops)
     {
         let cyclops = cyclops_q.get(*cyclops_entity);
         let enemy = enemy_q.get(*enemy_entity);
@@ -91,23 +91,25 @@ fn handle_archer_attack(
     archer_q: Query<(&Transform, &Damage), With<AllyType>>,
     enemy_q: Query<&Transform, With<EnemyType>>,
 ) {
-    if let Some(AttackEvent(_, archer_entity, enemy_entity)) =
-        attack_events.iter().find(|ev| ev.0 == AllyType::Archer)
+    for AttackEvent(_, archer_entity, enemy_entity) in
+        attack_events.iter().filter(|ev| ev.0 == AllyType::Archer)
     {
         let (transform, damage) = archer_q.get(*archer_entity).unwrap();
         let other_transform = enemy_q.get(*enemy_entity).unwrap();
 
+        let dir =
+            (other_transform.translation.truncate() - transform.translation.truncate()).normalize();
+
         commands.spawn_bundle(ProjectileBundle {
             speed: Speed(20.0),
-            velocity: Velocity(
-                (other_transform.translation.truncate() - transform.translation.truncate())
-                    .normalize(),
-            ),
+            velocity: Velocity(dir),
             damage: Damage(damage.0),
             projectile: Projectile::<AllyType>::default(),
             sprite: SpriteBundle {
                 texture: sprites.arrow.clone(),
-                transform: Transform::from_translation(transform.translation),
+                transform: Transform::from_translation(transform.translation)
+                    .with_rotation(Quat::from_rotation_z(Vec2::Y.angle_between(dir)))
+                    .with_scale(Vec3::splat(1.5)),
                 ..default()
             },
         });
@@ -119,8 +121,8 @@ fn handle_dwarf_attack(
     dwarf_q: Query<(&Transform, &Damage), With<AllyType>>,
     enemy_q: Query<&Transform, With<EnemyType>>,
 ) {
-    if let Some(AttackEvent(_, dwarf_entity, enemy_entity)) =
-        attack_events.iter().find(|ev| ev.0 == AllyType::Dwarf)
+    for AttackEvent(_, dwarf_entity, enemy_entity) in
+        attack_events.iter().filter(|ev| ev.0 == AllyType::Dwarf)
     {
         let dwarf = dwarf_q.get(*dwarf_entity);
         let enemy = enemy_q.get(*enemy_entity);
@@ -132,8 +134,8 @@ fn handle_knight_attack(
     knight_q: Query<(&Transform, &Damage), With<AllyType>>,
     enemy_q: Query<&Transform, With<EnemyType>>,
 ) {
-    if let Some(AttackEvent(_, knight_entity, enemy_entity)) =
-        attack_events.iter().find(|ev| ev.0 == AllyType::Knight)
+    for AttackEvent(_, knight_entity, enemy_entity) in
+        attack_events.iter().filter(|ev| ev.0 == AllyType::Knight)
     {
         let knight = knight_q.get(*knight_entity);
         let enemy = enemy_q.get(*enemy_entity);
@@ -145,8 +147,8 @@ fn handle_player_attack(
     player_q: Query<(&Transform, &Damage), With<AllyType>>,
     enemy_q: Query<&Transform, With<EnemyType>>,
 ) {
-    if let Some(AttackEvent(_, player_entity, enemy_entity)) =
-        attack_events.iter().find(|ev| ev.0 == AllyType::Player)
+    for AttackEvent(_, player_entity, enemy_entity) in
+        attack_events.iter().filter(|ev| ev.0 == AllyType::Player)
     {
         let player = player_q.get(*player_entity);
         let enemy = enemy_q.get(*enemy_entity);
@@ -160,8 +162,8 @@ fn handle_wizard_attack(
     wizard_q: Query<(&Transform, &Damage), With<AllyType>>,
     enemy_q: Query<&Transform, With<EnemyType>>,
 ) {
-    if let Some(AttackEvent(_, wizard_entity, enemy_entity)) =
-        attack_events.iter().find(|ev| ev.0 == AllyType::Wizard)
+    for AttackEvent(_, wizard_entity, enemy_entity) in
+        attack_events.iter().filter(|ev| ev.0 == AllyType::Wizard)
     {
         let (transform, damage) = wizard_q.get(*wizard_entity).unwrap();
         let other_transform = enemy_q.get(*enemy_entity).unwrap();
@@ -188,8 +190,8 @@ fn handle_bat_attack(
     bat_q: Query<(&Transform, &Damage), With<EnemyType>>,
     ally_q: Query<&Transform, With<AllyType>>,
 ) {
-    if let Some(AttackEvent(_, bat_entity, ally_entity)) =
-        attack_events.iter().find(|ev| ev.0 == EnemyType::Bat)
+    for AttackEvent(_, bat_entity, ally_entity) in
+        attack_events.iter().filter(|ev| ev.0 == EnemyType::Bat)
     {
         let bat = bat_q.get(*bat_entity);
         let ally = ally_q.get(*ally_entity);
@@ -203,9 +205,9 @@ fn handle_evil_wizard_attack(
     evil_wizard_q: Query<(&Transform, &Damage), With<EnemyType>>,
     ally_q: Query<&Transform, With<AllyType>>,
 ) {
-    if let Some(AttackEvent(_, evil_wizard_entity, ally_entity)) = attack_events
+    for AttackEvent(_, evil_wizard_entity, ally_entity) in attack_events
         .iter()
-        .find(|ev| ev.0 == EnemyType::EvilWizard)
+        .filter(|ev| ev.0 == EnemyType::EvilWizard)
     {
         let (transform, damage) = evil_wizard_q.get(*evil_wizard_entity).unwrap();
         let other_transform = ally_q.get(*ally_entity).unwrap();
@@ -213,9 +215,8 @@ fn handle_evil_wizard_attack(
         commands.spawn_bundle(ProjectileBundle {
             speed: Speed(20.0),
             velocity: Velocity(
-                (other_transform.translation.truncate()
-                    - transform.translation.truncate())
-                .normalize(),
+                (other_transform.translation.truncate() - transform.translation.truncate())
+                    .normalize(),
             ),
             damage: Damage(damage.0),
             projectile: Projectile::<EnemyType>::default(),
@@ -233,8 +234,8 @@ fn handle_ghost_attack(
     ghost_q: Query<(&Transform, &Damage), With<EnemyType>>,
     ally_q: Query<&Transform, With<AllyType>>,
 ) {
-    if let Some(AttackEvent(_, ghost_entity, ally_entity)) =
-        attack_events.iter().find(|ev| ev.0 == EnemyType::Ghost)
+    for AttackEvent(_, ghost_entity, ally_entity) in
+        attack_events.iter().filter(|ev| ev.0 == EnemyType::Ghost)
     {
         let ghost = ghost_q.get(*ghost_entity);
         let ally = ally_q.get(*ally_entity);
@@ -246,8 +247,8 @@ fn handle_lobster_attack(
     lobster_q: Query<(&Transform, &Damage), With<EnemyType>>,
     ally_q: Query<&Transform, With<AllyType>>,
 ) {
-    if let Some(AttackEvent(_, lobster_entity, ally_entity)) =
-        attack_events.iter().find(|ev| ev.0 == EnemyType::Lobster)
+    for AttackEvent(_, lobster_entity, ally_entity) in
+        attack_events.iter().filter(|ev| ev.0 == EnemyType::Lobster)
     {
         let lobster = lobster_q.get(*lobster_entity);
         let ally = ally_q.get(*ally_entity);
@@ -259,8 +260,8 @@ fn handle_rat_attack(
     rat_q: Query<(&Transform, &Damage), With<EnemyType>>,
     ally_q: Query<&Transform, With<AllyType>>,
 ) {
-    if let Some(AttackEvent(_, rat_entity, ally_entity)) =
-        attack_events.iter().find(|ev| ev.0 == EnemyType::Rat)
+    for AttackEvent(_, rat_entity, ally_entity) in
+        attack_events.iter().filter(|ev| ev.0 == EnemyType::Rat)
     {
         let rat = rat_q.get(*rat_entity);
         let ally = ally_q.get(*ally_entity);
@@ -272,8 +273,8 @@ fn handle_spider_attack(
     spider_q: Query<(&Transform, &Damage), With<EnemyType>>,
     ally_q: Query<&Transform, With<AllyType>>,
 ) {
-    if let Some(AttackEvent(_, spider_entity, ally_entity)) =
-        attack_events.iter().find(|ev| ev.0 == EnemyType::Spider)
+    for AttackEvent(_, spider_entity, ally_entity) in
+        attack_events.iter().filter(|ev| ev.0 == EnemyType::Spider)
     {
         let spider = spider_q.get(*spider_entity);
         let ally = ally_q.get(*ally_entity);
